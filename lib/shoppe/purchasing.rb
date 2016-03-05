38fea2738fb45d19f5096c68271e8d07132fdecf
David Nicol <david@sidstan.com>
2016-03-05 21:21:38 +0000

    Implement Stripe Subscription invoice.payment_succeeded API call and toggle to version 0.0.3

---
+++ lib/shoppe/purchasing.rb
@@ -0,0 +1,47 @@
+module Purchasing
+  def purchase(customer, subscriber, invoice)
+    product = subscriber.subscription_plan.product
+
+    ActiveRecord::Base.transaction do
+      # Allow exception to propogate up so that Stripe queues and retries, as that saves us
+      # having to build a queuing and failed order workflow, thanks Stripe.com.
+      order = Shoppe::Order.create(notes: "Created for Stripe invoice #{invoice.id}")
+      order.customer = customer
+
+      # All billing and delivery details need to be copied to the order. Shoppe requirement.
+      order.first_name = customer.first_name
+      order.last_name = customer.last_name
+
+      address = customer.addresses.first
+
+      order.billing_address1 = address.address1
+      order.billing_address2 = address.address2
+      order.billing_address3 = address.address3
+      order.billing_address4 = address.address4
+      order.billing_postcode = address.postcode
+      order.billing_country = address.country
+      order.delivery_name = customer.full_name
+      order.delivery_address1 = address.address1
+      order.delivery_address2 = address.address2
+      order.delivery_address3 = address.address3
+      order.delivery_address4 = address.address4
+      order.delivery_postcode = address.postcode
+      order.delivery_country = address.country
+      order.email_address = customer.email
+      order.phone_number = customer.phone
+
+      order.order_items.add_item(product, 1)
+
+      order.payments.create(amount: subscriber.subscription_plan.product.price,
+                            method: 'Subscription Reallocation',
+                            reference: subscriber.stripe_token,
+                            refundable: false,
+                            confirmed: true)
+
+      subscriber.update balance: subscriber.balance - subscriber.subscription_plan.product.price
+
+      order.proceed_to_confirm
+      order.confirm!
+    end
+  end
+end
--- lib/shoppe/stripe_events/invoice_payment_succeeded.rb
+++ lib/shoppe/stripe_events/invoice_payment_succeeded.rb
@@ -1,11 +1,28 @@
+require 'shoppe/purchasing'
+
 class InvoicePaymentSucceeded
+  include Purchasing
+
   def call(event)
     # Handle the incoming invoice succeeded webhook
 
-    customer = event.data.object.customer
+    invoice = event.data.object
 
-    # Add amount to subcriber_amount for relevant subscription
+    # We can't look up the subscriber object from customer, we have to record both the
+    # customer token and subscription token when creating the subscription, so we can
+    # look them back up here.
+    customer = Shoppe::Customer.find_by_stripe_token invoice.customer
+    subscriber = Shoppe::Subscriber.find_by_stripe_token invoice.subscription
+
+    # Add amount to balance for relevant subscription
+    if subscriber.present?
+      amount = invoice.total / 100.0
+      subscriber.update_attribute(:balance, (subscriber.balance + amount))
+    end
 
     # Auto order the product if the balance now matches (or exceeds) product cost
+    if subscriber.balance >= subscriber.subscription_plan.product.price
+      purchase(customer, subscriber, invoice)
+    end
   end
 end
