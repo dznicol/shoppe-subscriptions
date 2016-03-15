bcf0d658d98bd373a3366605d36aa5ceb50eaf2d
David Nicol <david@sidstan.com>
2016-03-15 22:50:57 +0000

    Wrap invoice_payment_succeeded webhook code in transaction

--- lib/shoppe/stripe_events/invoice_payment_succeeded.rb
+++ lib/shoppe/stripe_events/invoice_payment_succeeded.rb
@@ -4,37 +4,39 @@ class InvoicePaymentSucceeded
   include Purchasing
 
   def call(event)
-    # Handle the incoming invoice succeeded webhook
+    ActiveRecord::Base.transaction do
+      # Handle the incoming invoice succeeded webhook
 
-    invoice = event.data.object
+      invoice = event.data.object
 
-    # We can't look up the subscriber object from customer, we have to record both the
-    # customer token and subscription token when creating the subscription, so we can
-    # look them back up here.
-    customer = Shoppe::Customer.find_by_stripe_token invoice.customer
-    subscriber = Shoppe::Subscriber.find_by_stripe_token invoice.subscription
+      # We can't look up the subscriber object from customer, we have to record both the
+      # customer token and subscription token when creating the subscription, so we can
+      # look them back up here.
+      customer = Shoppe::Customer.find_by_stripe_token invoice.customer
+      subscriber = Shoppe::Subscriber.find_by_stripe_token invoice.subscription
 
-    # Add amount to balance for relevant subscription
-    if subscriber.present?
-      total = Shoppe::ApiHandler.native_amount invoice.total
-      subtotal = Shoppe::ApiHandler.native_amount invoice.subtotal
-      # Subtotal is "Total of all subscriptions, invoice items, and prorations on the invoice before any discount is applied".
-      # By using subtotal means we are taking into account any discount when deciding whether there are sufficient funs
-      # to trigger a purchase below.
-      subscriber.update_attribute(:balance, (subscriber.balance + subtotal))
+      # Add amount to balance for relevant subscription
+      if subscriber.present?
+        total = Shoppe::ApiHandler.native_amount invoice.total
+        subtotal = Shoppe::ApiHandler.native_amount invoice.subtotal
+        # Subtotal is "Total of all subscriptions, invoice items, and prorations on the invoice before any discount is applied".
+        # By using subtotal means we are taking into account any discount when deciding whether there are sufficient funs
+        # to trigger a purchase below.
+        subscriber.update_attributes!(balance: (subscriber.balance + subtotal))
 
-      discount_code = invoice.discount.present? ? invoice.discount.coupon.id : nil
+        discount_code = invoice.discount.present? ? invoice.discount.coupon.id : nil
 
-      # Record the transaction for accounting later
-      subscriber.transactions.create({total: total,
-                                      subtotal: subtotal,
-                                      discount_code: discount_code,
-                                      transaction_type: Shoppe::SubscriberTransaction::TYPES[0]})
-    end
+        # Record the transaction for accounting later
+        subscriber.transactions.create({total: total,
+                                        subtotal: subtotal,
+                                        discount_code: discount_code,
+                                        transaction_type: Shoppe::SubscriberTransaction::TYPES[0]})
+      end
 
-    # Auto order the product if the balance now matches (or exceeds) product cost
-    if subscriber.balance >= subscriber.subscription_plan.product.price
-      purchase(customer, subscriber, invoice)
+      # Auto order the product if the balance now matches (or exceeds) product cost
+      if subscriber.balance >= subscriber.subscription_plan.product.price
+        purchase(customer, subscriber, invoice)
+      end
     end
   end
 end
