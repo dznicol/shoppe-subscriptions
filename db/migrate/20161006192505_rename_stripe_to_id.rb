f2454c1435cb89bf481c8b17e807ff36ba2208ae
David Nicol <david@sidstan.com>
2016-10-06 20:51:17 +0100

    Rename stripe_token to stripe_id throughout

---
+++ db/migrate/20161006192505_rename_stripe_to_id.rb
@@ -0,0 +1,6 @@
+class RenameStripeToId < ActiveRecord::Migration
+  def change
+    rename_column :shoppe_subscribers, :stripe_token, :stripe_id
+    rename_column :shoppe_customers, :stripe_token, :stripe_id
+  end
+end
